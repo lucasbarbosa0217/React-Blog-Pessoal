f1a59c77a42a3b7c38ae35a38c2f0b44837fd2d7
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-08-10 12:25:07 -0300

    Design dos temas

--- src/components/Tema/deletarTema/DeletarTema.tsx
+++ src/components/Tema/deletarTema/DeletarTema.tsx
@@ -68,22 +68,22 @@ function DeletarTema() {
     return (
         <div className='bg-light-background3 m-4 dark:bg-dark-background3 flex flex-col p-4 max-w-[30rem] w-full self-center justify-center gap-2'>
             <h1 className='text-4xl font-serif text-center my-4'>Deletar tema</h1>
-            <p className='text-center  mb-4'>Você tem certeza de que deseja apagar o tema a seguir?</p>
+            <p className='text-center  mb-4'>Você tem certeza de que deseja apagar o tema a seguir? Todas as postagens relacionadas a esse tema serão <span className='font-black text-red-500'>apagadas para sempre.</span></p>
 
-            <div className='flex flex-col overflow-hidden justify-between shadow-md bg-light-background3 dark:bg-dark-background3'>
+            <div className='flex flex-col overflow-hidden justify-between shadow-md border dark:border-dark-background2  bg-light-background3 dark:bg-dark-background3'>
                 <header className='p-2 bg-light-accent text-dark-textContent '>
                     Tema
                 </header>
                 <p className='p-4 text-2xl text-center'>{tema.description}</p>
                 <div className="flex border-t-2 dark:border-dark-background2">
                     <button
-                        className='w-full  text-blue-500   py-2  border-r-2 dark:border-dark-background2'
+                        className='w-full  text-blue-500   py-2  border-r-2 dark:border-dark-background2  hover:bg-blue-500 hover:text-stone-50'
                         onClick={retornar}
                     >
                         Não
                     </button>
                     <button
-                        className='w-full flex items-center text-red-500 justify-center'
+                        className='w-full flex items-center text-red-500 justify-center font-black hover:bg-red-500 hover:text-stone-50'
                         onClick={deletarTema}
                     >
                         Sim
